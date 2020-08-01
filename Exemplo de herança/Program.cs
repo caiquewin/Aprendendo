b62e1519a9fe80668476238c447bec4bbb75ea3e
caiquewin@gmail.com <caiquewin@gmail.com>
2020-08-01 11:43:43 -0300

    UPCASTING EXEMPLO

--- Exemplo de herança/Program.cs
+++ Exemplo de herança/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Exemplo_de_herança.Entities;
+using System;
 
 namespace Exemplo_de_herança
 {
@@ -6,7 +7,16 @@ namespace Exemplo_de_herança
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            BussinesAccount c1 = new BussinesAccount(0001,"Caique Pereira Alve",10.000,20.000);
+            // UPCASTING 
+
+            Account c2 = c1;
+
+            //other ex
+
+            Account c4 = new BussinesAccount(0002, "Carlos Eduardo Pereira Alves", 50.000, 100.00);
+
+            Account C5 = new SavingsAccount(0003, "Paulo Henrique Pereira Alves", 100.000, 0.01);
         }
     }
 }
