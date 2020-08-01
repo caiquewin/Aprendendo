2b313b58b683e25598f570a3682eba5634937b2f
caiquewin@gmail.com <caiquewin@gmail.com>
2020-08-01 11:32:05 -0300

    Estudando sobre heraça polimorfismo upcasting

---
+++ Exemplo de herança/Entities/BussinesAccount.cs
@@ -0,0 +1,26 @@
+﻿
+
+namespace Exemplo_de_herança.Entities
+{
+    class BussinesAccount : Account
+    {
+        public double LoanLiumit { get; set; }
+
+        public BussinesAccount()
+        {
+
+        }
+
+        public BussinesAccount(int number, string holder, double balance, double loanLimit) : base(number, holder, balance)
+        {
+            LoanLiumit = loanLimit;
+        }
+        public void Loan(double amount)
+        {
+            if (amount <= LoanLiumit)
+            {
+                Balance += amount;
+            }
+        }
+    }
+}
---
+++ Exemplo de herança/Entities/SavingsAccount.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exemplo_de_herança.Entities
+{
+    class SavingsAccount
+    {
+    }
+}
